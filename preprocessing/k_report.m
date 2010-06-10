743e225609b897546aee88145aa1c5a7b1ac5f0a
Joshua Holbrook <josh@pidgey.(none)>
2010-06-10 12:32:48 -0800

    cleaned up some olde files

--- preprocessing/k_report.m
+++
@@ -1,8 +0,0 @@
-function k_report(k_mats_props, filename)
-    %takes k_mats_props and generates a human-readable report.
-    %k_report(k_mats_props, filename)
-    fopen('k_report.txt', 'w');
-    for i=1:size(k_mats_props,1),
-        for j=1:size(k_mats_props,2),
-            f
-end
