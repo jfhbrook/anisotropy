7d14acb422d5a9c223fd46bf7dd1980bc0f600fc
Joshua Holbrook <josh@pidgey.(none)>
2010-06-10 14:56:04 -0800

    array2cell and cell2array also duplicate some functionality

--- matlab_fp/cell2array.m
+++ matlab_fp/cell2array.m
@@ -1,9 +1,2 @@
 function A=cell2array(a)
-    A=zeros(size(a));
-    %converts an array to a 2-d cell structure
-    for m=1:size(a,1),
-        for n=1:size(a,2),
-            A(m,n)=a{m,n};
-        end
-    end
-end
+    A=cell2mat(a)
