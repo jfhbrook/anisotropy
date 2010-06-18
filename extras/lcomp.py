70e3631a43de55a32e98540b3c33fe0170c70bb1
Joshua Holbrook <josh.holbrook@gmail.com>
2010-06-18 15:35:34 -0800

    spruced up lcomp a bit

--- extras/lcomp.py
+++ extras/lcomp.py
@@ -8,7 +8,12 @@ def iter_ij(A):
     """
     return reduce(lambda x,y: x+y, ([[(i,j) for j in xrange(3)] for i in xrange(3)]))
 
+def acomp(A,xs):
+    return array(xs).reshape(A.shape)
+    
+
+
 if __name__=="__main__":
     A=array(range(9)).reshape(3,3)
     print A
-    print array([A[i,j] for (i,j) in iter_ij(A)]).reshape(A.shape)
+    print acomp(A, [A[i,j] for (i,j) in iter_ij(A)])
