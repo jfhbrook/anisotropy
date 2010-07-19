4b8b8d9748ce304cce324c21464286dad37c1f65
Joshua Holbrook <josh.holbrook@gmail.com>
2010-07-19 10:19:51 -0800

    listDerivative now fits to lower order polynomials, and both listDer and lagrange use polyfit now

--- post/listDerivative.m
+++ post/listDerivative.m
@@ -1,23 +1,24 @@
-function fprime=listDerivative(x,f,n)
-    % Returns the nth derivative of f with respect to x, 
-    % where zip(x,f) are datapoints using lagrange
-    % interpolating polynomials using o points.
-    % This is a port of a similar function which I wrote in python.
+function [fprime,x]=listDerivative(f,x,n)
+    % [fprime,x]=listDerivative(f,x,n)
+    % Returns f' for the nth derivative of f with respect to x, 
+    % where zip(x,f) are datapoints using interpolating polyfits.
 
-    o=5; %Should make this an optional arg
-    assert(mod(o,2) == 1); %should be odd imo
+    %Ideally would be opts
+    npts=7;
+    o=3;
+    assert(o < npts);
     derivative=[];
     for i=1:length(f)
-        start=i-(o-1)/2;
+        start=i-(npts-1)/2;
         if start < 1,
             start = 1;
         end
-        theend=start+o;
+        theend=start+npts;
         if theend >= length(f),
             theend = length(f);
-            start = theend-o;
+            start = theend-npts;
         end
-        spline=lagrange(x(start:theend),f(start:theend));
+        spline=polyfit(x(start:theend),f(start:theend),o);
         for i=1:n
             spline=polyder(spline);
         end
