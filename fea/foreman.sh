c477bfbb597f1a8325d949c6a4d858402a18e49f
Joshua Holbrook <josh.holbrook@gmail.com>
2010-08-03 10:39:16 -0800

    foreman and worker now incorporate old babbysitter stuff because comsol still segfaults :(

--- fea/foreman.sh
+++ fea/foreman.sh
@@ -4,4 +4,36 @@
 . /u1/uaf/holbrook/.profile
 cd /scratch/holbrook/fea
 
-nohup comsol matlab -np 4 -ml -nosplash -ml -nodisplay -r "worker($@)" &
+logfile="babbysit.log"
+lockfile="sim.lock"
+
+if [ -f $lockfile ]; then
+    if [ "$(ps -p `cat $lockfile` | wc -l)" -gt 1 ]; then
+        "Dude, it's already running! wtf" >> $logfile
+        exit
+    fi
+fi
+
+nohup comsol -np 4 matlab -ml -nodesktop -ml -nosplash -mlr "worker($@)" >> $logfile &
+comPID=$!
+echo "Comsol Matlab PID: $comPID " >> $logfile
+echo $comPID > $lockfile
+
+#as long as it's supposedly running....
+while [ -f $lockfile ]; do
+    sleep 10m #we don't need to check THAT often... 
+    #checks for the existence of a segfault
+    if "tail -n 2 $logfile | grep -c 'Segmentation fault'" ; then
+        echo "Sumbitch segfaulted! Restarting..." > $logfile
+        nohup comsol -np 4 matlab -ml -nodesktop -ml -nosplash -mlr "worker($@)" >> $logfile &
+        comPID=$!
+        echo $comPID > $lockfile
+        echo "restarted.\n" >> $logfile
+    #maybe the program's done running?
+    elif [ "$(ps -p `cat $lockfile` | wc -l)" -lt 2 ]; then
+        echo "PROCESS STOPPED WITHOUT A SEGFAULT--MAYBE ITS DONE!" >> $logfile
+        cat $logfile | mutt -s'Hey, stuff happened.' josh.holbrook@gmail.com
+        exit
+    fi
+done
+
--- fea/worker.m
+++ fea/worker.m
@@ -6,32 +6,46 @@ function worker(angles)
 %Runs FEA jobs for the supplied list of angles.
 
     flreport('off');
-
-    ks = linspace(0.2,0.4,8);
-    %ks = linspace(0.2,0.4,2);
-    [kxy,kz] = meshgrid(ks,ks);
-    % Some parameters we won't want to iterate through
-    params=struct('rsnow', 0.4, ...
-                  'rneedle', 0.0005, ...
-                  'lneedle', 0.1, ...
-                  'density_snow', 200, ...
-                  'density_needle', 8000, ...
-                  'cp_snow', 2050, ...
-                  'cp_needle', 460, ...
-                  'q_needle', 0.5, ...
-                  'k_needle', 160, ...
-                  'time', [logspace(0.1,1,15) logspace(1,3,15)], ...
-                  'saveroot', './solutions/');
-
-    %note: angles in degrees!
-    for angle=angles,
-        % mesh
-        mesh = mesher(angle,params);
-        % fea
-        solutions = arrayfun(@(x,y) solver(x,y,mesh,params), kxy,kz, 'UniformOutput', false);
-        % curve fit
-        solutions = cellfun(@(x) {x{1} x{2} fitter(x{1}(1,:),x{1}(2,:))},solutions);
-        save([params.saveroot 'solution-' date '-' num2str(angle)],'solutions','angle','ks','params');
-        system(['tar -rf /archive/u1/uaf/holbrook/fea_sols.tar ' params.saveroot 'solution-' date '-' num2str(angle) '.mat']);
+    %initialization
+    if (exist('feasols.mat')~=2),
+        ks = linspace(0.2,0.4,8);
+        %ks = linspace(0.2,0.4,2);
+        [kxy,kz] = meshgrid(ks,ks);
+        % Some parameters we won't want to iterate through
+        params=struct('rsnow', 0.4, ...
+                      'rneedle', 0.0005, ...
+                      'lneedle', 0.1, ...
+                      'density_snow', 200, ...
+                      'density_needle', 8000, ...
+                      'cp_snow', 2050, ...
+                      'cp_needle', 460, ...
+                      'q_needle', 0.5, ...
+                      'k_needle', 160, ...
+                      'time', [logspace(0.1,1,15) logspace(1,3,15)]);
+        mesh = NaN*angles;
+        solutions = num2cell(NaN*kxy);
+        save feasols;
+    else,
+        load feasols;
+        %note: angles in degrees!
+        for i=1:length(angles),
+            % mesh
+            if (mesh(i)==NaN),
+                mesh(i) = mesher(angles(i),params);
+                save feasols;
+            end
+            % fea
+            for j=1:shape(solutions,1)*shape(solutions,2),
+                if (solutions{j}==NaN),
+                    solutions{j} = solver(kxy(j),kz(j),mesh,params);
+                    % curve fit
+                    solutions{j} = {solutions{j} fitter(solutions{j}{1}(1,:),solutions{j}{1}(2,:))};
+                    save feasols;
+                else,
+                    fprintf "Already did that one.";
+                end
+            end
+            save feasols;
+        end
     end
 end
