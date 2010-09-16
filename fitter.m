0830c169630c129d014a4bf4b2e8a443a99326da
Joshua Holbrook <josh.holbrook@gmail.com>
2010-09-16 16:42:17 -0700

    Now gives some basic feedback to indicate that *something* is happening as it fits.

--- fitter.m
+++ fitter.m
@@ -6,6 +6,7 @@ function k = fitter(t,T,Q)
     opts.TolX = 10e-11;
     opts.Display = 'off';
 
+    fprintf('|');
     k = lsqcurvefit(@ (x,a) x(1).*real(expint(x(2)./a)),[0.05 -0.0001],t(t>0),T(t>0),[],[],opts);
 
 end
