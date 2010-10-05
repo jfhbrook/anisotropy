7362101efe9d6d9098a13a34ee04cab44fd2739d
Joshua Holbrook <josh.holbrook@gmail.com>
2010-10-04 18:43:07 -0700

    The fitter wasn't fucking finished! What the Hell happened? RAGE

--- fitter.m
+++ fitter.m
@@ -1,4 +1,5 @@
-function k = fitter(t,T,Q)
+function k = fitter(t,T,params)
+
     opts = optimset('lsqcurvefit');
     opts.MaxFunEvals = 10e5;
     opts.MaxIter = 10e5;
@@ -7,6 +8,7 @@ function k = fitter(t,T,Q)
     opts.Display = 'off';
 
     fprintf('|');
-    k = lsqcurvefit(@ (x,a) x(1).*real(expint(x(2)./a)),[0.05 -0.0001],t(t>0),T(t>0),[],[],opts);
+    x = lsqcurvefit(@ (x,a) x(1).*real(expint(x(2)./a)),[0.05 -0.0001],t(t>0),T(t>0),[],[],opts);
+    k = (params.q_needle * params.density_snow * params.cp_snow)/(4*pi*x(1));
 
 end
