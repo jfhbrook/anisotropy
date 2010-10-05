781682f596277e29b6baea4cd7b74ce8f4aadd25
Joshua Holbrook <josh.holbrook@gmail.com>
2010-10-04 19:43:15 -0800

    I think it's k, not kappa.

--- fitter.m
+++ fitter.m
@@ -1,4 +1,5 @@
-function k = fitter(t,T,Q)
+function k = fitter(t,T,params)
+
     opts = optimset('lsqcurvefit');
     opts.MaxFunEvals = 10e5;
     opts.MaxIter = 10e5;
@@ -7,6 +8,10 @@ function k = fitter(t,T,Q)
     opts.Display = 'off';
 
     fprintf('|');
-    k = lsqcurvefit(@ (x,a) x(1).*real(expint(x(2)./a)),[0.05 -0.0001],t(t>0),T(t>0),[],[],opts);
+    x = lsqcurvefit(@ (x,a) x(1).*real(expint(x(2)./a)),[0.05 -0.0001],t(t>0),T(t>0),[],[],opts);
+    plot(log(t),T);
+    hold on;
+    plot(log(t), x(1)*log(t));
+    k = (params.q_needle)/(4*pi*x(1));
 
 end
