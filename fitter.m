e98d2a4347527e49821ef21c88b43ac909c3e079
Joshua Holbrook <josh.holbrook@gmail.com>
2010-10-04 20:56:19 -0800

    turns out the expint fit was crap. Using corrcoef to find linear portion now

--- fitter.m
+++ fitter.m
@@ -1,17 +1,21 @@
 function k = fitter(t,T,params)
+    logt = log(t(t>1));
+    T = T(t>1);
 
-    opts = optimset('lsqcurvefit');
-    opts.MaxFunEvals = 10e5;
-    opts.MaxIter = 10e5;
-    opts.TolFun = 10e-11;
-    opts.TolX = 10e-11;
-    opts.Display = 'off';
+    disp('Finding linear portion...');    
+    for i=1:length(logt)-1
+        C = corrcoef(logt(i:length(logt)), T(i:length(T)));
+        r = sqrt(C(2,1));
+        if r > 0.995 %adjust this to get 'good' values
+            disp(['linear fitting to ' num2str((length(logt)-i)) ' points from t=' num2str(exp(logt(i))) ' to t=' num2str(exp(logt(length(logt)))) '...']);
+            x = polyfit(logt(i:length(logt)),T(i:length(T)), 1);
+            break
+        end
+    end
 
-    fprintf('|');
-    x = lsqcurvefit(@ (x,a) x(1).*real(expint(x(2)./a)),[0.05 -0.0001],t(t>0),T(t>0),[],[],opts);
-    plot(log(t),T);
-    hold on;
-    plot(log(t), x(1)*log(t));
+    %plot(logt,T,'*');
+    %hold on;
+    %plot(logt, x(1)*logt + x(2));
     k = (params.q_needle)/(4*pi*x(1));
 
 end
