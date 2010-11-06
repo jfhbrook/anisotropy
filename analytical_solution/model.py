92c58b562570e2bb0ee35caf6d257e9d5c6663d4
Joshua Holbrook <josh.holbrook@gmail.com>
2010-11-05 17:34:50 -0800

    Hey, seems to work! Just need to plot the results now. >_<

--- analytical_solution/model.py
+++ analytical_solution/model.py
@@ -1,5 +1,5 @@
 from math import pi
-from numpy import log, sin, cos, sqrt, array, arange, linspace, dot
+from numpy import log, sin, cos, sqrt, array, arange, hstack, linspace, dot
 from functools import reduce
 #from matplotlib import pyplot as plt
 
@@ -7,9 +7,13 @@ def elliptical(fxn, ecc):
     from scipy.integrate import quad #Do I want quad?
     from math import pi
     from numpy import sin, cos, sqrt
-    if type(fxn) == int:
-        fxn = lambda ecc, th: fxn
-    return quad(lambda th: fxn(ecc,th) *
+    from types import FunctionType, BuiltinFunctionType
+    if type(fxn) != FunctionType and type(fxn) != BuiltinFunctionType:
+        fxn2 = lambda ecc, th: fxn
+    else:
+        fxn2 = fxn
+
+    return quad(lambda th: fxn2(ecc,th) *
                                sqrt( cos(th)**2.0 + (ecc*sin(th))**2.0 ),
                 0, 2*pi)[0]
 
@@ -18,19 +22,21 @@ def Tavg(k_x, k_y, q, t):
     given scalar r0, k_x, k_y and 1-d time, this returns a curve with the same 
     slope at Tavg(t) for long T. May refactor.
     """
-    return (4*pi*k_x/q)*array([elliptical(log(t) , k_y/k_x)/elliptical(1, k_y/k_x) for time in t])
+    return (4*pi*k_x/q)*array([elliptical(log(time) , k_y/k_x)/elliptical(1, k_y/k_x) for time in t])
 
 def kmeas(k_x, k_y, q, t):
     from numpy import polyfit
     """
     Does a quick linear curve fit 
     """
+    #print('Finding k_meas...')
     return (q/4/pi/k_x)*polyfit(t, log(Tavg(k_x, k_y, q, t)), 1)[0]
 
 #Do I even need this?
 def rot(th, axis):
     from numpy.linalg import norm
     from numpy import sin, cos, eye, outer, cross
+    #print('Generating matrix...')
     if axis == "x":
         axis = array([1,0,0])
     elif axis == "y":
@@ -46,17 +52,17 @@ def rot(th, axis):
 #Want to port some of these ideas to proj, but I suck at stuff
 #Probably don't even actually want to do this, really, but whatevs
 def proj(threespace, twospace):
+    #print('Projecting onto twospace...')
     #Using the normalized two-space as
     from numpy import diag
     from numpy.linalg import norm, eig
     twospace = dot(twospace, diag([1.0/norm(twospace.T[i]) for i in xrange(twospace.shape[0])]))
-    print(twospace)
     #kind of a lisp-ish indent pattern, eh? >_<
     #Could probably make this more efficient.
     return eig(dot(twospace, 
                    diag(dot(array([1,1,1]), 
                             dot(pad(twospace, threespace.shape), 
-                                threespace))[0:twospace.shape[0]])))
+                                threespace))[0:twospace.shape[0]])))[0]
 
 def pad(matrix, mn):
     """
@@ -79,10 +85,11 @@ def trim(matrix, mn):
 if __name__=="__main__":
     from numpy import arange, diag, logspace, meshgrid
     from math import pi
+    from progressbar import ProgressBar
 
     angles = arange(90) #Lots angles :D
 
-    ks = arange(0.2, 0.05, 0.4) # Some ks
+    ks = arange(0.2, 0.4, 0.05) # Some ks
     [k_xy, k_z] = meshgrid(ks, ks)
     k_xy = k_xy.flatten()
     k_z = k_z.flatten()
@@ -91,21 +98,22 @@ if __name__=="__main__":
     t = hstack(( logspace(0.1,1.0,15),
                  logspace(1.0,3.0,15) ))
 
-    #TODO:
-    # 1) angles = arange(some bullshit)
-    # 3) Find 2-d problem for x'z' plane
-    # 4) Find that k-meas as functions of different k_x and k_y
-    # 5) Output those solutions!
-
-    for th in angles:
+    results = [[]]
+    progress = ProgressBar()
+    for th in progress(angles):
+        #print('Angle: '+ str(th))
         for i in xrange(ks.shape[0]):
+            #print('k_xy = '+str(k_xy[i])+' and k_z = '+str(k_z[i])+': ')
             (k_xp, k_yp) = proj( diag([k_xy[i], k_xy[i], k_z[i]]),
-                                 rot(pi/180*th, 'y') )
+                                 rot(pi/180*th, 'y')[1::,1::] )
+            #print('k_xp = '+str(k_xp))
+            #print('k_yp = '+str(k_yp))
             results.append([ th,
                              k_xy,
                              k_z,
                              k_xp,
                              k_yp,
                              kmeas(k_xp, k_yp, q, t)])
+            #print('Done.')
+    #print(results)
 
-    print(results)
