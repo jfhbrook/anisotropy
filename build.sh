a0a1b7b01af4b21314a3b1dbaeda691ab574c61c
Joshua Holbrook <josh.holbrook@gmail.com>
2011-04-06 13:17:59 -0800

    Fairly certain no new sections

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ pdflatex --shell-escape thesis
 # http://tex.stackexchange.com/questions/8211/list-of-appendices (2nd answer)
 # and fix, for everyone else's benefit.
 # This will only work automagically if NO SECTIONS ARE ADDED OR DELETED btw
-# cp thesis.toc.bk thesis.toc
-# cp thesis.loa.bk thesis.loa
+cp thesis.toc.bk thesis.toc
+cp thesis.loa.bk thesis.loa
 pdflatex --shell-escape thesis
