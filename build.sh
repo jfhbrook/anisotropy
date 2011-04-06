e4d935a9ace8c30e3c157a9a7b6a190b79a3fb0a
Joshua Holbrook <josh.holbrook@gmail.com>
2011-04-05 23:45:59 -0800

    A stupid hack to make separate appendices work for now.

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@ pdflatex --shell-escape thesis
 pdflatex --shell-escape thesis
 bibtex thesis
 pdflatex --shell-escape thesis
+cp thesis.toc.bk thesis.toc
+cp thesis.loa.bk thesis.loa
 pdflatex --shell-escape thesis
