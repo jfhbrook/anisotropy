84c7a9bc06a68c9a735940e91c5855714ca9da38
Joshua Holbrook <josh.holbrook@gmail.com>
2011-04-05 23:53:53 -0800

    Decided copying shouldnt be standard, and wrote a note.

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ pdflatex --shell-escape thesis
 # Stupid hack to get separate appendices to work. In future, look at:
 # http://tex.stackexchange.com/questions/8211/list-of-appendices (2nd answer)
 # and fix, for everyone else's benefit.
-cp thesis.toc.bk thesis.toc
-cp thesis.loa.bk thesis.loa
+# This will only work automagically if NO SECTIONS ARE ADDED OR DELETED btw
+# cp thesis.toc.bk thesis.toc
+# cp thesis.loa.bk thesis.loa
 pdflatex --shell-escape thesis
